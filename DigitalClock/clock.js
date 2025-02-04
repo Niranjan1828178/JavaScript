5d4c7a25356f1c0d77edc91c80688b20fc31abf7
niranjan <niranjan64461231@gmail.com>
2025-02-04 19:24:24 +0530

    clock

---
+++ DigitalClock/clock.js
@@ -0,0 +1,13 @@
+function Clock(){
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours=hours<10  ? "0"+hours : hours;
+    minutes=minutes<10 ? "0"+minutes : minutes;
+    seconds=seconds<10 ? "0"+seconds : seconds;
+    let time=hours+":"+minutes+":"+seconds;
+    document.querySelector("#clock").innerText = time;
+}
+setInterval(Clock,100)
